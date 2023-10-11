4a80a2d149099262ee257cf4531293b1d916f45e
Camilo Valencia <cvalencia@prescient.com>
2023-10-11 14:43:07 -0400

    added parse

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 import ProductListing from './Components/ProductListing/ProductListing';
+import Parse from "parse";
+
+Parse.serverURL = "https://parseapi.back4app.com"; // This is your Server URL
+// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
+Parse.initialize(
+  "8s5HtEE4TCs3Eai05y0lHPMlL7kBPP1daPu9pL5i", // This is your Application ID
+  "J5IkXss5RKPsBqaM6lzHJxFmgMtbB2NWOqVxHJ58" // This is your Javascript key
+);
 
 function App() {
   return (
