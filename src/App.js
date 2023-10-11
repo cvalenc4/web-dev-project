5782b7baf04d9cea4c7666be0ad1ebe42f4ebdbc
Camilo Valencia <cvalencia@prescient.com>
2023-10-11 15:03:26 -0400

    create env variables for parse

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import ProductListing from './Components/ProductListing/ProductListing';
+import * as Env from "./environments";
 import Parse from "parse";
 
-Parse.serverURL = "https://parseapi.back4app.com"; // This is your Server URL
-// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
-Parse.initialize(
-  "8s5HtEE4TCs3Eai05y0lHPMlL7kBPP1daPu9pL5i", // This is your Application ID
-  "J5IkXss5RKPsBqaM6lzHJxFmgMtbB2NWOqVxHJ58" // This is your Javascript key
-);
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
 
 function App() {
   return (
---
+++ src/environments.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: "8s5HtEE4TCs3Eai05y0lHPMlL7kBPP1daPu9pL5i",
+    JAVASCRIPT_KEY: "J5IkXss5RKPsBqaM6lzHJxFmgMtbB2NWOqVxHJ58",
+    SERVER_URL: "https://parseapi.back4app.com/"
+};
